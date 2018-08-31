8d136288bd9fdf1e6052a9803e0abb0ae3d47de3
djow <31188129+djowdobom@users.noreply.github.com>
2018-08-31 19:36:19 -0300

    Create br.lua

---
+++ locales/br.lua
@@ -0,0 +1,8 @@
+Locales['br'] = {
+  ['whitelist_check']     = 'tendo certeza que você está na whitelist . . .',
+  ['not_whitelisted']     = 'você não está na whitelist deste servidor!',
+  ['steamid_error']       = 'sua Steam ID não foi encontrada, está com a steam aberta?',
+  ['whitelist_empty']     = 'a whitelist ainda não foi carregada, ou ninguém está nela!',
+  ['help_whitelist_add']  = 'adicionar alguém a whitelist',
+  ['help_whitelist_load'] = 'carregar a whitelist',
+}
